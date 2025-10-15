adeb5bcb98a4fc034c750253ce5e4e451ce8227c
Koffi-Fredysessie <koffisessie@gmail.com>
2025-10-15 22:35:32 +0200

    Initial commit - BRVM API project

--- BRVM_scraper.py
+++ BRVM_scraper.py
@@ -34,7 +34,21 @@ def BRVM_get(symbols, from_date=None, to_date=None, verbose=True):
         raise ValueError("No valid tickers after filtering against Symbole list.")
 
     returns = pd.DataFrame(columns=["Date","Open","High","Low","Close","Volume","Ticker"])
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'fr,fr-FR;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+    'priority': 'u=0, i',
+    'referer': 'https://www.richbourse.com/common/mouvements/technique',
+    'sec-ch-ua': '"Microsoft Edge";v="141", "Not?A_Brand";v="8", "Chromium";v="141"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36 Edg/141.0.0.0',
+    }
 
     for tick in symbol_vec:
         if verbose:
